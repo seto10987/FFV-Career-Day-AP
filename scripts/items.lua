e49c170135ab93742fdb0b6ff235f4ae1e0e7416
seto10987 <62239408+seto10987@users.noreply.github.com>
2024-06-02 16:06:05 -0500

    Delete scripts directory

--- scripts/items.lua
+++
@@ -1,9 +0,0 @@
-Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/bosses.json")
-Tracker:AddItems("items/hosted_items.json")
-Tracker:AddItems("items/jobs.json")
-Tracker:AddItems("items/abilities.json")
-Tracker:AddItems("items/spells.json")
-
-
-
--- scripts/layouts.lua
+++
@@ -1,6 +0,0 @@
-Tracker:AddLayouts("layouts/item_grids.json")
-Tracker:AddLayouts("layouts/job_grid.json")
-Tracker:AddLayouts("layouts/tracker_layouts.json")
-Tracker:AddLayouts("layouts/broadcast.json")
-
-
